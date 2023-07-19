ba1541d0a2303d7040284982e8dabb517da66197
goodrbiflock <businessintel@goodr.com>
2023-07-18 17:20:37 -0700

    working table script

--- js/script.js
+++ js/script.js
@@ -9,3 +9,58 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+
+  function parseCSV(csvData) {
+    const lines = csvData.split('\n');
+    const data = [];
+    for (let i = 0; i < lines.length; i++) {
+        const values = lines[i].split(',');
+        data.push(values);
+    }
+    return data;
+}
+
+function generateTable(data) {
+    const tableContainer = document.getElementById('tableContainer');
+    const table = document.createElement('table');
+
+    for (let i = 0; i < data.length; i++) {
+        const row = document.createElement('tr');
+
+        for (let j = 0; j < data[i].length; j++) {
+            const cell = document.createElement(i === 0 ? 'th' : 'td');
+            cell.textContent = data[i][j];
+            row.appendChild(cell);
+        }
+
+        table.appendChild(row);
+    }
+
+    tableContainer.appendChild(table);
+}
+
+function loadData(fileName) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'assets/' + fileName, true); // Update the path to the CSV files
+
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const csvData = xhr.responseText;
+            const parsedData = parseCSV(csvData);
+            generateTable(parsedData);
+        } else {
+            console.error('Failed to load the CSV file: ' + fileName);
+        }
+    };
+
+    xhr.onerror = function() {
+        console.error('Failed to load the CSV file: ' + fileName);
+    };
+
+    xhr.send();
+}
+
+window.onload = function() {
+    const fileName = 'unionedrevprev.csv'; // Replace with the desired CSV file name
+    loadData(fileName);
+};
